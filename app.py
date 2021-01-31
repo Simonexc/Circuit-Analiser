a495ec3d39a69fe786b8498b14138871bdf0adfc
Szymon Trochimiak <szymekdt@gmail.com>
2021-02-01 00:37:04 +0100

    POST and wire routing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 
 app = Flask(__name__)
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 files = [
     "/static/images/dot.svg",
+    "/static/images/wire.svg",
     "/static/images/resistor.svg",
     "/static/images/capacitor.svg",
     "/static/images/inductor.svg",
@@ -13,14 +15,23 @@ files = [
     "/static/images/i_source.svg",
     "/static/images/intersection.svg"
 ]
-x, y = 10, 5
+x, y = 5, 4
 counters = [0 for f in files]
-labels = ["", "R", "C", "L", "E", "I", ""]
+labels = ["", "", "R", "C", "L", "E", "I", ""]
+rotation_modes = [0, 0, 2, 2, 2, 1, 1, 0]
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_func():
-    return render_template("layout.html", files=files, x=x, y=y, counters=counters, labels=labels)
+    if request.method == 'POST':
+        grid = request.data
+        print(grid)
+        response = app.response_class(response=json.dumps(['aa', 2]),
+                                      status=200,
+                                      mimetype='application/json')
+        return response
+    return render_template("layout.html", files=files, x=x, y=y, counters=counters, labels=labels,
+                           rotation_modes=rotation_modes)
 
 
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -6,7 +6,13 @@ var sources = [];
 var counters = [];
 var labels = [];
 var changes = [];
-var rotation = [];
+var rotation = 0;
+var rotation_modes = []
+
+function rotate()
+{
+    rotation = (rotation+90)%360;
+}
 
 function createData()
 {
@@ -17,7 +23,7 @@ function createData()
         for (j = 0; j < x; j++)
         {
             counters[0]++;
-            l.push([0, counters[0]]);
+            l.push([0, counters[0], 0]);
             l_changes.push(true);
         }
         data.push(l);
@@ -25,50 +31,143 @@ function createData()
     }
 }
 
-function addDrawings(act_val, drawingsDiv, opacity, rect)
+function createImage(act_val, rect)
+{
+    var img = document.createElement("img");
+    img.src = sources[act_val]
+    img.style.position = "absolute";
+    img.style.width = "50px";
+    img.style.height = "50px";
+    //img.style.top = rect.top.toString()+"px";
+    //img.style.left = rect.left.toString()+"px";
+
+    return img;
+}
+
+function addDrawings(act_val, drawingsDiv, opacity, rect, rot, x1, y1)
 {
     drawingsDiv.innerHTML = "";
 
-    var img = document.createElement("img");
-    img.src = sources[act_val]
-    img.class = "abs_img";
-    img.style.top = rect.top.toString()+"px";
-    img.style.left = rect.left.toString()+"px";
-    img.style.opacity = opacity.toString();
-    drawingsDiv.appendChild(img);
+    if (act_val == 1) {
+        if (act_val == sources.length - 1)
+            console.log(x1 + " " + y1);
+        var count_legs = 0;
+        if (y1 > 0) {
+            var p = data[y1-1][x1];
+            if (p[0] == 1 || p[0] == sources.length - 1 || (p[0] != 0 && (p[2] == 0 || p[2] == 180))) {
+                var img = createImage(act_val, rect);
+                img.style.opacity = opacity.toString();
+                drawingsDiv.appendChild(img);
+                count_legs++;
+            }
+        }
+        if (y1 < y-1) {
+            var p = data[y1+1][x1];
+            if (p[0] == 1 || p[0] == sources.length - 1 || (p[0] != 0 && (p[2] == 0 || p[2] == 180))) {
+                var img = createImage(act_val, rect);
+                img.style.opacity = opacity.toString();
+                img.style.transform = "rotate(180deg)";
+                drawingsDiv.appendChild(img);
+                count_legs++;
+            }
+        }
+        if (x1 > 0) {
+            var p = data[y1][x1-1];
+            if (p[0] == 1 || p[0] == sources.length - 1 || (p[0] != 0 && (p[2] == 90 || p[2] == 270))) {
+                var img = createImage(act_val, rect);
+                img.style.opacity = opacity.toString();
+                img.style.transform = "rotate(270deg)";
+                drawingsDiv.appendChild(img);
+                count_legs++;
+            }
+        }
+        if (x1 < x-1) {
+            var p = data[y1][x1+1];
+            if (p[0] == 1 || p[0] == sources.length - 1 || (p[0] != 0 && (p[2] == 90 || p[2] == 270))) {
+                var img = createImage(act_val, rect);
+                img.style.opacity = opacity.toString();
+                img.style.transform = "rotate(90deg)";
+                drawingsDiv.appendChild(img);
+                count_legs++;
+            }
+        }
+        if (count_legs > 2) {
+            var img = createImage(sources.length - 1, rect);
+            img.style.opacity = opacity.toString();
+            drawingsDiv.appendChild(img);
+        }
+    }
+    else {
+        var img = createImage(act_val, rect);
+        img.style.opacity = opacity.toString();
+        if (rotation_modes[act_val] == 2) {
+            if (rot == 90 || rot == 270) {
+                img.style.transform = "rotate(270deg)";
+            }
+        }
+        else if (rotation_modes[act_val] == 1) {
+            img.style.transform = "rotate("+rot+"deg)";
+        }
+
+        drawingsDiv.appendChild(img);
+    }
+}
+
+function updateNumbers(x_pos, y_pos)
+{
+    act_val = data[y_pos][x_pos][0];
+    count_val = data[y_pos][x_pos][1];
+    counters[act_val]--;
+
+    for (var i = 0; i < y; i++)
+    {
+        for (var j = 0; j < x; j++)
+        {
+            if (data[i][j][0] == act_val && data[i][j][1] > count_val)
+            {
+                data[i][j][1]--;
+                changes[i][j] = true;
+            }
+        }
+    }
 }
 
 function updateBoard()
 {
-    for (i = 0; i<y; i++)
+    for (var i = 0; i<y; i++)
     {
-        for (j = 0; j<x; j++)
+        for (var j = 0; j<x; j++)
         {
-            if (!changes[i][j])
+            var act_val = data[i][j][0];
+            if (!changes[i][j] && act_val != 1)
                 continue;
-            changes[i][j] = false;
             var id = i*100+j;
             id = id.toString();
-            var act_val = data[i][j][0];
-
+            /*
+            cell = document.getElementById("c"+id);
+            cell.style.top = (150+i*50)+"px";
+            cell.style.left = (200+j*50)+"px";
+            */
             element = document.getElementById("i"+id);
             var rect = element.getBoundingClientRect();
 
             labelDiv = document.getElementById("l"+id);
-            labelDiv.style.top = rect.top.toString()+"px";
-            labelDiv.style.left = rect.left.toString()+"px";
+            //labelDiv.style.top = rect.top.toString()+"px";
+            //labelDiv.style.left = rect.left.toString()+"px";
             labelDiv.innerHTML = "";
             if (labels[act_val] != "")
             {
-                labelDiv.innerHTML = "$"+labels[act_val]+"_"+data[i][j][1]+"$"
+                labelDiv.innerHTML = "$"+labels[act_val]+"_"+data[i][j][1]+"$";
                 MathJax.Hub.Queue(["Typeset",MathJax.Hub,labelDiv]);
             }
 
             drawingsDiv = document.getElementById("d"+id);
-            drawingsDiv.style.top = rect.top.toString()+"px";
-            drawingsDiv.style.left = rect.left.toString()+"px";
+            //drawingsDiv.style.top = rect.top.toString()+"px";
+            //drawingsDiv.style.left = rect.left.toString()+"px";
 
-            addDrawings(act_val, drawingsDiv, 1, rect);
+            addDrawings(act_val, drawingsDiv, 1, rect, data[i][j][2], j, i);
+
+            changes[i][j] = false;
         }
     }
 }
@@ -78,25 +177,12 @@ function clickButton(id)
     y_pos = Math.floor(id/100);
     x_pos = id%100;
 
-    act_val = data[y_pos][x_pos][0];
-    count_val = data[y_pos][x_pos][1];
-    counters[act_val]--;
-
-    for (i = 0; i < y; i++)
-    {
-        for (j = 0; j < x; j++)
-        {
-            if (data[i][j][0] == act_val && data[i][j][1] > count_val)
-            {
-                data[i][j][1]--;
-                changes[i][j] = true;
-            }
-        }
-    }
+    updateNumbers(x_pos, y_pos);
 
     counters[val]++;
     data[y_pos][x_pos][0] = val;
     data[y_pos][x_pos][1] = counters[val];
+    data[y_pos][x_pos][2] = rotation;
     changes[y_pos][x_pos] = true;
 
     updateBoard();
@@ -105,11 +191,24 @@ function onMouse(id)
 {
     drawingsDiv = document.getElementById("d"+id);
     var rect = drawingsDiv.getBoundingClientRect();
-    addDrawings(val, drawingsDiv, 0.4, rect);
+    addDrawings(val, drawingsDiv, 0.4, rect, rotation, id%100, Math.floor(id/100));
 }
 function offMouse(id)
 {
     drawingsDiv = document.getElementById("d"+id);
     var rect = drawingsDiv.getBoundingClientRect();
-    addDrawings(data[Math.floor(id/100)][id%100][0], drawingsDiv, 1, rect);
+    var componentData = data[Math.floor(id/100)][id%100]
+    addDrawings(componentData[0], drawingsDiv, 1, rect, componentData[2], id%100, Math.floor(id/100));
+}
+
+function sendData()
+{
+    fetch("/", {
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
 }
